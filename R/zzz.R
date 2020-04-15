9b29bb85fa53bc567b347c7e46c099f764379275
David Holstius <david.holstius@gmail.com>
2020-04-15 11:22:04 -0700

    change .onLoad() to .onAttach()

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,21 @@
-.onLoad <- function(libname, pkgname) {
+.onAttach <- function (
+  libname,
+  pkgname
+) {
 
-  today_is <- paste0("Today is ", base::date())
-  utils::timestamp(today_is, prefix = "##------ [welcome] ", quiet = TRUE)
-  packageStartupMessage(today_is)
+  today_is <-
+    paste0(
+      "Today is ",
+      base::date())
+
+  utils::timestamp(
+    today_is,
+    prefix = "##------ [welcome] ",
+    quiet = TRUE)
+
+  packageStartupMessage(
+    today_is)
+
+  return(TRUE)
 
 }
