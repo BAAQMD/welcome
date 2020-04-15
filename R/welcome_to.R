2e628f92bae4ba98d56daaa9e8979d3a16292bc3
David Holstius <david.holstius@gmail.com>
2020-04-15 11:10:29 -0700

    import utils::packageVersion()

--- R/welcome_to.R
+++ R/welcome_to.R
@@ -2,10 +2,12 @@
 #'
 #' @include packageTimestamp.R
 #'
+#' @importFrom utils packageVersion
+#'
 #' @export
 welcome_to <- function (pkg_name) {
 
-  pkg_ver <- packageVersion(pkg_name)
+  pkg_ver <- utils::packageVersion(pkg_name)
   pkg_ts <- packageTimestamp(pkg_name)
   pkg_msg <- sprintf("This is %s v%s (%s)", pkg_name, pkg_ver, pkg_ts)
 
