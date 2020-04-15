37965ceeeee122f88fc85163fce054433afd21ec
David Holstius <david.holstius@gmail.com>
2020-04-15 11:22:18 -0700

    fix documentation for packageTimestamp()

--- R/packageTimestamp.R
+++ R/packageTimestamp.R
@@ -1,28 +1,45 @@
+#' packageTimestamp
+#'
+#' Extract the "Built" timestamp from a package's description.
+#'
+#' @param pkgname (character) package name
+#'
 #' @importFrom lubridate ymd_hms with_tz
 #' @importFrom stringr str_extract
+#' @importFrom utils packageDescription
+#'
 #' @export
-packageTimestamp <- function (pkgname) {
+packageTimestamp <- function (
+  pkgname
+) {
 
-  pkg_desc <- packageDescription(pkgname)
+  pkg_description <-
+    utils::packageDescription(pkgname)
 
-  pkg_timestamp <-
+  if("Built" %in% names(pkg_description)) {
 
-    if("Built" %in% names(pkg_desc)) {
+    timestamp_pattern <-
+      "[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} UTC"
 
-      pkg_built <- pkg_desc[["Built"]]
-      locale_tz <- Sys.timezone()
+    pkg_built <-
+      pkg_description[["Built"]]
 
-      timestamp_pattern <- "[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} UTC"
-      pkg_built_timestamp <- str_extract(pkg_built, timestamp_pattern)
+    pkg_built_timestamp <-
+      stringr::str_extract(
+        pkg_built,
+        timestamp_pattern)
 
-      with_tz(ymd_hms(pkg_built_timestamp), tz = "UTC")
+    pkg_timestamp <-
+      lubridate::with_tz(
+        ymd_hms(pkg_built_timestamp),
+        tzone = "UTC")
 
+  } else {
 
-    } else {
-
+    pkg_timestamp <-
       invisible(NULL)
 
-    }
+  }
 
   return(pkg_timestamp)
 
